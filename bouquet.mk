02a86859b9069eb6f39a852fe909da8366f7b0ca
sabarop <sabar.pakpahan@gmail.com>
2025-05-27 21:59:07 +0700

    sdm660-common: Enable UFFD GC
    Kernel has support for it now.
    
    Co-Authored-By: Michael Bestas <mikeNG@users.noreply.github.com>

--- bouquet.mk
+++ bouquet.mk
@@ -326,6 +326,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
