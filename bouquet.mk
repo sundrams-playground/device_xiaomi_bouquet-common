bbabd489dd0b1a113dbb1b89f76b6549a2d24d13
xyz-sundram <sundaramxd@outlook.com>
2026-06-04 15:12:34 +0000

    Revert "sdm660-common: overlay: add DeviceAsWebcam overlay"
    This reverts commit 3a457b1d2dcfc9b5a74b678ff631f0c4e21d07d6.

--- bouquet.mk
+++ bouquet.mk
@@ -356,9 +356,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_PACKAGES += \
-    SDM660DeviceAsWebcam
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Power
