692d18f8347b0abe1e01656a437a69a77f164607
xyz-sundram <sundaramxd@outlook.com>
2026-06-04 11:16:56 +0000

    sdm660-common: commonize usb

--- bouquet.mk
+++ bouquet.mk
@@ -448,6 +448,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.basic
+
 # Updater
 AB_OTA_UPDATER := false
 
